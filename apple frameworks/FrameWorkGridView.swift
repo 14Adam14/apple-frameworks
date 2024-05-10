5839f9c21ac75e2bad34679e09e1d8ff6c965be8
Adam Kaum <adam.bolotov@gmail.com>
2024-05-11 01:35:22 +0400

    m

---
+++ apple frameworks/FrameWorkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameWorkGridView.swift
+//  apple frameworks
+//
+//  Created by Adam Kaum on 11.05.2024.
+//
+
+import SwiftUI
+
+struct FrameWorkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameWorkGridView()
+}
---
+++ apple frameworks/Framework.swift
@@ -0,0 +1,8 @@
+//
+//  Framework.swift
+//  apple frameworks
+//
+//  Created by Adam Kaum on 11.05.2024.
+//
+
+import Foundation
