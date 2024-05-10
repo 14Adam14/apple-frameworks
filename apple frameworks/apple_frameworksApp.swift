ee866927be5ee6ff25b331634dd56efcc2bf4476
Adam Kaum <adam.bolotov@gmail.com>
2024-05-10 21:23:06 +0400

    Initial Commit

---
+++ apple frameworks/apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworksApp.swift
+//  apple frameworks
+//
+//  Created by Adam Kaum on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
