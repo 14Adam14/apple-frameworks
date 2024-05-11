207064ba9acabad290742f1db9dcecf19d384459
Adam Kaum <adam.bolotov@gmail.com>
2024-05-11 05:57:42 +0400

    add view

--- apple frameworks/FrameworkDetailView.swift
+++ apple frameworks/FrameworkDetailView.swift
@@ -1,5 +1,5 @@
 //
-//  FrameworkDataView.swift
+//  FrameworkDetailView.swift
 //  apple frameworks
 //
 //  Created by Adam Kaum on 11.05.2024.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct FrameworkDataView: View {
+struct FrameworkDetailView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    FrameworkDataView()
+    FrameworkDetailView()
 }
