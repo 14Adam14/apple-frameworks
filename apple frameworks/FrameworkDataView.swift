c683f3aced2444567e2bb5750319e51df1265e03
Adam Kaum <adam.bolotov@gmail.com>
2024-05-11 04:33:13 +0400

    m

---
+++ apple frameworks/FrameworkDataView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDataView.swift
+//  apple frameworks
+//
+//  Created by Adam Kaum on 11.05.2024.
+//
+
+import SwiftUI
+
+struct FrameworkDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDataView()
+}
